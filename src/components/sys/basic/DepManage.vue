960f2c0596ca1f245a89e44007b4721415ba8636
lwg729 <1098372951@qq.com>
2020-12-28 23:14:38 +0800

    部门管理/部门展示

--- src/components/sys/basic/DepManage.vue
+++ src/components/sys/basic/DepManage.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        部门管理
+    <div style="width: 500px">
+        <el-input
+                placeholder="输入关键字进行过滤"
+                prefix-icon="el-icon-search"
+                v-model="filterText">
+        </el-input>
+
+        <el-tree style="margin-top: 10px"
+                class="filter-tree"
+                :data="deps"
+                :props="defaultProps"
+                :filter-node-method="filterNode"
+                ref="tree">
+        </el-tree>
     </div>
 </template>
 
 <script>
     export default {
-        name: "DepManager"
+        name: "DepManager",
+        data() {
+            return {
+                filterText: '',
+                deps: [],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                },
+            }
+        },
+        mounted() {
+            this.initDeps();
+        },
+        watch: {
+            filterText(val) {
+                this.$refs.tree.filter(val);
+            }
+        },
+        methods: {
+            initDeps() {
+                this.getRequest("/system/basic/department/").then(resp => {
+                    if (resp) {
+                        this.deps = resp;
+                    }
+                })
+            },
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.label.indexOf(value) !== -1;
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
